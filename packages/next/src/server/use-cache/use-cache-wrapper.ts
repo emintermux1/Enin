83275a3c6f8edc7083cff92d57c4a190e96f7b9a
conico974 <nicodorseuil@yahoo.fr>
2025-02-16 18:28:16 +0100

    fix dynamicIO for On Demand revalidation

--- packages/next/src/server/use-cache/use-cache-wrapper.ts
+++ packages/next/src/server/use-cache/use-cache-wrapper.ts
@@ -917,5 +917,11 @@ function shouldForceRevalidate(
     }
   }
 
+  // If it's an on-demand revalidation and not a stale revalidation request
+  // then we should force revalidation to mimic the behavior of unstable_cache.
+  if (workStore.isOnDemandRevalidate && !workStore.isStaleRevalidationRequest) {
+    return true
+  }
+
   return false
 }
